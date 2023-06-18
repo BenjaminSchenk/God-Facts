010c45c391e537a82a35b2fb4e9915aec5d6b1b0
BenjaminSchenk <benjamin.k.schenk@gmail.com>
2023-06-18 15:17:33 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -4,5 +4,5 @@ const allBtn = document.querySelector("#Gods")
 allBtn.addEventListener('click', async () => {
     const response = await fetch('https://ancient-gods.onrender.com/gods')
     const data = response.json()
-    console.log(data.result)
+    console.log(data)
 })
