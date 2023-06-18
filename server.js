b277fa6e2642c4b68e523dfa0de9cbb2e5e81068
BenjaminSchenk <benjamin.k.schenk@gmail.com>
2023-06-18 13:18:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -42,11 +42,35 @@ app.get('/gods/:name', async (req, res) => {
 app.get('/pantheon/:name', async (req, res) => {
     const { name } = req.params
     try {
-        const result = await pool.query('SELECT gods.name, gods.god_goddess_of, gods.info, gods.fun_facts, gods.pantheon_name, pantheon.description FROM gods INNER JOIN pantheon ON gods.pantheon_name = pantheon.name WHERE TRIM(pantheon.name) = $1', [name])
+        const result = await pool.query('SELECT gods.name, gods.god_goddess_of, gods.info, gods.fun_facts, gods.pantheon_name, pantheon.description FROM gods INNER JOIN pantheon ON gods.pantheon_name = pantheon.name WHERE pantheon.name = $1', [name])
         res.status(200).send(result.rows)
     } catch (err) {
         console.error(err)
         res.status(500).send('Internal Server Error.')
+    }
+})
+
+app.post('/god', async (req, res) => {
+    const { name, god_goddess_of, info, fun_facts, pantheon_name } = req.body
+    try {
+        const result = await pool.query('INSERT INTO gods (name, god_goddess_of, info, fun_facts, pantheon_name) VALUES ($1, $2, $3, $4, $5)', [name, god_goddess_of, info, fun_facts, pantheon_name])
+        res.status(201).send('New god added.')
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+
+
+app.delete('/god/:name', async (req, res) => {
+    const { name } = req.params
+    try {
+        const result = pool.query('DELETE FROM gods WHERE name = $1', [name])
+        res.status(201).send('God has been deleted')
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Internal server error.')
     }
 })
 
