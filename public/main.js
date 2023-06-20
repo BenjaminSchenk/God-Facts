ab6676e39ec904ea484cf8bee17d79b953fb5108
BenjaminSchenk <benjamin.k.schenk@gmail.com>
2023-06-19 19:51:01 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -59,12 +59,12 @@ hindu.addEventListener('click', async () => {
     pantheon(data)
 })
 
-dele.addEventListener('click', async () => {
-    const response = fetch(`https://ancient-gods.onrender.com/gods/${name}`, {
-        method: 'DELETE'
-      })
+//dele.addEventListener('click', async () => {
+  //  const response = fetch(`https://ancient-gods.onrender.com/gods/${name}`, {
+    //    method: 'DELETE'
+  //    })
 
-})
+//})
 console.log('working')
 searchBtn.addEventListener('click', async () => {
     const input = document.querySelector('#searchInput')
