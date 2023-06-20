fd1eb667f46ed72b8a657c9c5a53cdc2a99b0ba9
BenjaminSchenk <benjamin.k.schenk@gmail.com>
2023-06-20 09:21:40 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -85,6 +85,9 @@ form.addEventListener('submit', (event) => {
     }
     fetch('https://ancient-gods.onrender.com/gods', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(obj)
     })
 })
